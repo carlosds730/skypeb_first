afc2edd0c30dac3496c5d790b35727ac1dae660c
Carlos <carlos_ds730@yahoo.es>
2016-02-01 17:06:20 -0500

    first commit

--- skypeb/models.py
+++ skypeb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Newsletter_Clients(models.Model):
+    class Meta:
+        verbose_name = 'Cliente de noticias'
+        verbose_name_plural = 'Clientes de noticias'
+
+    email = models.EmailField(verbose_name='Email', unique=True, help_text='Email al que mandar las noticias')
+
+    def __str__(self):
+        return str(self.email)
--- skypeb/views.py
+++ skypeb/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 # Create your views here.
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from skypeb import models
+from django.core.validators import validate_email
+import json
 
 
 def product_list(request):
+    if request.is_ajax():
+        try:
+            mail = request.GET['mail']
+
+            validate_email(mail)
+
+            m, _ = models.Newsletter_Clients.objects.get_or_create(email=mail)
+            m.save()
+            return HttpResponse(json.dumps({'status': 1}), content_type='application/json')
+        except KeyError:
+            return HttpResponse(json.dumps({'status': 0}), content_type='application/json')
+        except Exception as ee:
+            return HttpResponse(json.dumps({'status': 0}), content_type='application/json')
     return render(request, 'index.html')
